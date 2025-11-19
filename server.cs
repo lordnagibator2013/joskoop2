a4cb95e4105a44922536a701dd604e6fdb7de24a
lordnagibator2013 <kekynkek2308@email.com>
2025-11-19 22:23:12 +1000

    chatmessage

--- server.cs
+++ server.cs
@@ -12,18 +12,18 @@ public class ChatServer
     private TcpListener _listener;
     private bool _isRunning;
     private List<TcpClient> _connectedClients;
-    
+
     public ChatServer(int port = 8888)
     {
         _listener = new TcpListener(IPAddress.Any, port);
         _connectedClients = new List<TcpClient>();
     }
-    
+
     public async Task StartAsync()
     {
         _listener.Start();
         _isRunning = true;
-        
+
         while (_isRunning)
         {
             try
@@ -31,7 +31,7 @@ public class ChatServer
                 TcpClient client = await _listener.AcceptTcpClientAsync();
                 _connectedClients.Add(client);
                 Console.WriteLine($"ещё один, всего бродяг {_connectedClients.Count}");
-                
+
                 _ = HandleClientAsync(client);
             }
             catch (Exception ex)
@@ -40,9 +40,25 @@ public class ChatServer
             }
         }
     }
-    
+
     private async Task HandleClientAsync(TcpClient client)
     {
-        
+
+    }
+}
+
+public class ChatMessage
+{
+    private string type { get; set; }
+    private string text { get; set; }
+    private string userName { get; set; }
+    private DateTime Timestamp { get; set; }
+
+    public ChatMessage(string type, string userName, string text)
+    {
+        this.type = type;
+        this.userName = userName;
+        this.text = text;
+        Timestamp = DateTime.Now;
     }
 }
