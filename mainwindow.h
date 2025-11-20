d12cb406ddbf394a6e356717ae6d8665725254b4
Mazafaker <brawlstars7069@gmail.com>
2025-11-21 00:52:07 +1000

    добавил чат с самим собой

--- mainwindow.h
+++ mainwindow.h
@@ -4,24 +4,37 @@
 #include <QStackedWidget>
 #include <QPushButton>
 #include <QLineEdit>
+#include <QVBoxLayout>
+#include <QMap>
+#include <QDateTime>
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = nullptr);
 
-private:
-    QWidget *createChatsPage();
-    QWidget *createSettingsPage();
-    QWidget *createProfilePage();
+private slots:
+    void switchToChats();
+    void switchToSettings();
+    void switchToProfile();
+    void openMeChat();
+    void sendMessage();
 
+private:
     QStackedWidget *stack;
     QPushButton *chatsButton;
     QPushButton *settingsButton;
     QPushButton *profileButton;
 
-private slots:
-    void switchToChats();
-    void switchToSettings();
-    void switchToProfile();
+    QWidget *chatPage;
+    QVBoxLayout *messageLayout;
+    QLineEdit *messageEdit;
+
+    QList<QString> meMessages;
+    QMap<QString, QDateTime> chatActivity;
+
+    QWidget *createChatsPage();
+    QWidget *createSettingsPage();
+    QWidget *createProfilePage();
+    void refreshChatList();
 };
